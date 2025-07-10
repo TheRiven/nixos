8b87e158781ab59274f84d80ea1331672c00a7fe
James <novajames276@gmail.com>
2025-07-10 16:51:52 +0100

    config grub for nixtop

--- hosts/nixtop/configuration.nix
+++ hosts/nixtop/configuration.nix
@@ -24,6 +24,8 @@
   # Bootloader.
   #boot.loader.systemd-boot.enable = true;
   boot.loader.grub.enable = true;
+  boot.loader.grub.version = 2;
+  boot.loader.grub.device = "/dev/sda";
   boot.loader.efi.canTouchEfiVariables = true;
 
   # Use latest kernel.
