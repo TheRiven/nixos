22f4ef3fe1a98722369cebf0940c3c540f47a4ad
James <novajames276@gmail.com>
2025-07-11 15:08:16 +0100

    add nvidia licence acceptance

--- hosts/nixtop/configuration.nix
+++ hosts/nixtop/configuration.nix
@@ -71,6 +71,7 @@
   # Graphics Card settings
   hardware.graphics.enable = true;
   hardware.nvidia.open = false;
+  nixpkgs.config.nvidia.acceptLicense = true;
   hardware.nvidia.package = config.boot.kernelPackages.nvidiaPackages.legacy_390;
   services.xserver.videoDrivers = ["nvidia"];
   
