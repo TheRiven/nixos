c7e8594b0050f225627592a96d98576f37eabc10
James <novajames276@gmail.com>
2025-07-11 16:14:07 +0100

    Removed legacy drivers for test

--- hosts/nixtop/configuration.nix
+++ hosts/nixtop/configuration.nix
@@ -72,7 +72,7 @@
   hardware.graphics.enable = true;
   hardware.nvidia.open = false;
   nixpkgs.config.nvidia.acceptLicense = true;
-  hardware.nvidia.package = config.boot.kernelPackages.nvidiaPackages.legacy_390;
+  #hardware.nvidia.package = config.boot.kernelPackages.nvidiaPackages.legacy_390;
   services.xserver.videoDrivers = ["nvidia"];
   
   ## --- SERVICES -- ##
