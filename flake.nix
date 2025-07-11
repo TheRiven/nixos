31093d7e47daf6f0f2e334acc3150c0ea3d40dc6
James <novajames276@gmail.com>
2025-07-11 10:24:45 +0100

    fix to flake - hopefully

--- flake.nix
+++ flake.nix
@@ -7,26 +7,25 @@
   };
 
   outputs = { self, nixpkgs, ... }@inputs: {
-    nixosConfigurations = {
-      nixbox = nixpkgs.lib.nixosSystem {
-        specialArgs = {inherit inputs;};
-        modules = [
-          ./hosts/nixbox/configuration.nix
-          ./modules/locals.nix
-          ./modules/programs.nix
-          ./modules/users.nix
-        ];
-      };
-
-      nixtop = nixpkgs.lib.nixosSystem {
-        specialArgs = {inherit inputs;};
-        modules = [
-          ./hosts/nixtop/configuration.nix
-          ./modules/locals.nix
-          ./modules/programs.nix
-          ./modules/users.nix
-        ];
-      };
+    nixosConfigurations.nixbox = nixpkgs.lib.nixosSystem {
+      specialArgs = {inherit inputs;};
+      modules = [
+        ./hosts/nixbox/configuration.nix
+        ./modules/locals.nix
+        ./modules/programs.nix
+        ./modules/users.nix
+      ];
     };
+
+    nixosConfigurations.nixtop = nixpkgs.lib.nixosSystem {
+      specialArgs = {inherit inputs;};
+      modules = [
+        ./hosts/nixtop/configuration.nix
+        ./modules/locals.nix
+        ./modules/programs.nix
+        ./modules/users.nix
+      ];
+    };
+
   };
 }
