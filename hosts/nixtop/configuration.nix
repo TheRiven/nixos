1e9b1c6bb7b14c8fa30323f90195fbbfc5b2110f
James <novajames276@gmail.com>
2025-07-11 16:57:15 +0100

    remove nvidia for now

--- hosts/nixtop/configuration.nix
+++ hosts/nixtop/configuration.nix
@@ -70,10 +70,10 @@
 
   # Graphics Card settings
   hardware.graphics.enable = true;
-  hardware.nvidia.open = false;
-  nixpkgs.config.nvidia.acceptLicense = true;
+  #hardware.nvidia.open = false;
+  # nixpkgs.config.nvidia.acceptLicense = true;
   #hardware.nvidia.package = config.boot.kernelPackages.nvidiaPackages.legacy_390;
-  services.xserver.videoDrivers = ["nvidia"];
+  #services.xserver.videoDrivers = ["nvidia"];
   
   ## --- SERVICES -- ##
 
