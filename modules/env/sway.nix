1920db17a1fcc2bf629be9fcf7d923c6cbc437c9
James <novajames276@gmail.com>
2025-07-18 10:33:18 +0100

    add uwsm for sway

--- modules/env/sway.nix
+++ modules/env/sway.nix
@@ -22,12 +22,24 @@
         };
 
         programs.thunar.enable = true; # File Manager
-        #programs.seahorse.enable = true; # Gnome Keyring GUI
+        programs.seahorse.enable = true; # Gnome Keyring GUI
         programs.waybar.enable = true; # Waybar!
         #programs.nm-applet.enable = true; # GUI app for Networkmanager
 
-        #services.gnome.gnome-keyring.enable = true;
-        services.passSecretService.enable = true;
+        programs.uwsm = {
+            enable = true;
+            waylandCompositors = {
+                sway = {
+                    prettyName = "Sway";
+                    comment = "UWSM managed Sway";
+                    binPath = "/run/current-system/sw/bin/sway";
+                };
+            };
+
+        };
+
+        services.gnome.gnome-keyring.enable = true;
+        #services.passSecretService.enable = true;
 
         #security.pam.services.login.kwallet.enable = true;
         #security.pam.services.login.enableGnomeKeyring = true;
