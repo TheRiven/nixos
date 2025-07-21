775628ccc5d54e0c3ec88629727b8a60d1acb1c1
James <novajames276@gmail.com>
2025-07-21 11:19:01 +0100

    readd nm-applet and remove old KDE code

--- modules/env/sway.nix
+++ modules/env/sway.nix
@@ -24,6 +24,7 @@
         programs.thunar.enable = true; # File Manager
         programs.seahorse.enable = true; # Gnome Keyring GUI
         programs.waybar.enable = true; # Waybar!
+        programs.nm-applet.enable = true;
 
         programs.uwsm = {
             enable = true;
@@ -38,8 +39,6 @@
         };
 
         services.gnome.gnome-keyring.enable = true;
-
-        #security.pam.services.login.kwallet.enable = true;
         #security.pam.services.login.enableGnomeKeyring = true;
 
         services.displayManager.sddm.enable = true;
@@ -65,9 +64,6 @@
             wl-clipboard # Copy/Paste functions - Sway
             mako # Notification system - Sway
             wofi # Alternative Program launcher
-            #kdePackages.kwallet # Password locker
-            #kdePackages.kwallet-pam # Pam Kwallet integration
-            #kdePackages.kwalletmanager # Kwallet manager
             cmus # console music player
         ];
 
