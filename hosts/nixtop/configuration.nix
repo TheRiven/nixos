b371e82a49b0e3cc2fbae828bd51036d4a665a93
James <novajames276@gmail.com>
2025-07-29 09:46:17 +0100

    add openSSH to nixtop

--- hosts/nixtop/configuration.nix
+++ hosts/nixtop/configuration.nix
@@ -41,7 +41,7 @@
   networking.networkmanager.enable = true;
   
   # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
+  services.openssh.enable = true;
 
   # Open ports in the firewall.
   # networking.firewall.allowedTCPPorts = [ ... ];
